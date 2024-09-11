6a1bfec74951c7f66a191e7831295f9b1fc4d7e2
Jot Hogben <jot_hogben@hotmail.co.uk>
2024-09-11 14:22:11 +0100

    Javascript - change units and add value display

--- rangeSlider.js
+++ rangeSlider.js
@@ -1,4 +1,8 @@
+const rangeValue = document.getElementById('rangeValue');
+rangeValue.textContent = 0;
+
 function valueChanged(e) {
   let a = e.value;
-  e.style.background = `linear-gradient(to right,#4BD663,#4BD663 ${a}%,#eee ${a}%)`;
+  rangeValue.textContent = a;
+  e.style.background = `linear-gradient(to right,#4BD663,#4BD663 ${a * 5}%,#eee ${a * 5}%)`;
 }
