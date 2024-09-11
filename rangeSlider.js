2125f380cc6d1c6518c7f44546c85db6c551493f
Jot Hogben <jot_hogben@hotmail.co.uk>
2024-09-11 15:22:46 +0100

    Javascript, CSS - custom properties for slider

--- rangeSlider.js
+++ rangeSlider.js
@@ -1,9 +1,14 @@
 const rangeValue = document.getElementById('rangeValue');
 rangeValue.textContent = 0;
 const passwordMax = 100 / parseFloat(document.getElementById('lengthSlider').getAttribute('max'));
+const rootStyles = getComputedStyle(document.documentElement);
+const sliderProgress = rootStyles.getPropertyValue('--clr-1').trim();
+const sliderEmpty = rootStyles.getPropertyValue('--clr-2').trim();
 
 function rangeSliderColors(e) {
   let a = e.value;
   rangeValue.textContent = a;
-  e.style.background = `linear-gradient(to right,#4BD663,#4BD663 ${a * passwordMax}%,#eee ${a * passwordMax}%)`;
+  e.style.background = `linear-gradient(to right,${sliderProgress},${sliderProgress} ${
+    a * passwordMax
+  }%,${sliderEmpty} ${a * passwordMax}%)`;
 }
