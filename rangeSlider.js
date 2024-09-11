e6576b421144bac9123ee2cecd8e8d56a2f3b1e6
Jot Hogben <jot_hogben@hotmail.co.uk>
2024-09-11 14:44:38 +0100

    Javascript - passwordMax variable

--- rangeSlider.js
+++ rangeSlider.js
@@ -1,8 +1,9 @@
 const rangeValue = document.getElementById('rangeValue');
 rangeValue.textContent = 0;
+const passwordMax = 100 / parseFloat(document.getElementById('lengthSlider').getAttribute('max'));
 
-function valueChanged(e) {
+function rangeSliderColors(e) {
   let a = e.value;
   rangeValue.textContent = a;
-  e.style.background = `linear-gradient(to right,#4BD663,#4BD663 ${a * 5}%,#eee ${a * 5}%)`;
+  e.style.background = `linear-gradient(to right,#4BD663,#4BD663 ${a * passwordMax}%,#eee ${a * passwordMax}%)`;
 }
